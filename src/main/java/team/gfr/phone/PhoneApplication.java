dff56890c4810259ba86819d385b6f48352d2242
Kevin <773116395@qq.com>
2021-02-06 09:47:39 +0800

    SpringBoot+项目PhoneShop

--- src/main/java/team/gfr/phone/PhoneApplication.java
+++ src/main/java/team/gfr/phone/PhoneApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Kevin
+ */
 @SpringBootApplication
 @MapperScan("team.gfr.phone.dao")
 public class PhoneApplication {
